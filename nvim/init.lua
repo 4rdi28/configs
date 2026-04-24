645945b4cb6274990c5010bb3389f2fe16882408
4rdi28 <a_saberi@icloud.com>
2026-04-24 18:21:55 +0200

    remove conflicting custom alt bindings

--- nvim/init.lua
+++ nvim/init.lua
@@ -20,35 +20,6 @@ vim.keymap.set("v", "<C-c>", '"+y', { noremap = true, silent = true })
 -- NOTE: `map` affects normal/visual/operator modes; in Lua we can mimic that
 vim.keymap.set({ "n", "v", "o" }, "<C-v>", '"+P', { noremap = true, silent = true })
 
--- use alt to switch between windows
-
-local opts = { noremap = true, silent = true }
-
--- Normal mode: just move window focus
-vim.keymap.set("n", "<A-h>", "<C-w>h", opts)
-vim.keymap.set("n", "<A-j>", "<C-w>j", opts)
-vim.keymap.set("n", "<A-k>", "<C-w>k", opts)
-vim.keymap.set("n", "<A-l>", "<C-w>l", opts)
-
--- Insert mode: run one normal command (<C-w>h/j/k/l) then return to insert
-vim.keymap.set("i", "<A-h>", "<C-o><C-w>h", opts)
-vim.keymap.set("i", "<A-j>", "<C-o><C-w>j", opts)
-vim.keymap.set("i", "<A-k>", "<C-o><C-w>k", opts)
-vim.keymap.set("i", "<A-l>", "<C-o><C-w>l", opts)
-
--- Visual mode: exit visual, then move focus
-vim.keymap.set("v", "<A-h>", "<Esc><C-w>h", opts)
-vim.keymap.set("v", "<A-j>", "<Esc><C-w>j", opts)
-vim.keymap.set("v", "<A-k>", "<Esc><C-w>k", opts)
-vim.keymap.set("v", "<A-l>", "<Esc><C-w>l", opts)
-
--- Terminal mode: leave terminal-mode, then move focus (documented pattern)
-vim.keymap.set("t", "<A-h>", "<C-\\><C-n><C-w>h", opts)
-vim.keymap.set("t", "<A-j>", "<C-\\><C-n><C-w>j", opts)
-vim.keymap.set("t", "<A-k>", "<C-\\><C-n><C-w>k", opts)
-vim.keymap.set("t", "<A-k>", "<C-\\><C-n><C-w>k", opts)
-vim.keymap.set("t", "<A-l>", "<C-\\><C-n><C-w>l", opts)
-
 -- unbind <c-l> in terminal
 vim.api.nvim_create_autocmd("TermOpen", {
   pattern = "*",
